8ab350cb82784200d3a23340424b83817f564917
Abdelrahman Elbanna <abdelrhmanelbanna35@gmail.com>
2025-10-01 06:39:11 +0300

    handle media items

--- domain/src/main/java/com/example/domain/usecase/SendMessageUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SendMessageUseCase.kt
@@ -4,6 +4,11 @@ import com.example.domain.entity.Message
 import com.example.domain.repository.MessageRepository
 
 class SendMessageUseCase(private val repository: MessageRepository) {
-    suspend operator fun invoke(message: Message): Result<Unit> =
-        repository.sendMessage(message)
+    suspend operator fun invoke(message: Message): Result<Unit> {
+
+        if (message.mediaUrls.size > 10) {
+            return Result.failure(IllegalArgumentException("Cannot send more than 10 media items"))
+        }
+        return repository.sendMessage(message)
+    }
 }
